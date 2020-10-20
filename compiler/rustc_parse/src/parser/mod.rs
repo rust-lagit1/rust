e4a9b8b57405259d7fd5bb9e5f84635e595db68e
Camelid <camelidcamel@gmail.com>
2020-10-20 16:48:04 -0700

    Suggest adding missing braces in `const` block pattern
    Previously it would only suggest wrapping the code in braces in regular
    expressions; now it does it in patterns too.
    
    This is a squashed rebase of #78173

--- compiler/rustc_parse/src/parser/mod.rs
+++ compiler/rustc_parse/src/parser/mod.rs
@@ -1302,12 +1302,11 @@ impl<'a> Parser<'a> {
         }
     }
 
-    /// Parses inline const expressions.
+    /// Parses inline const expressions. The `const` keyword was already eaten.
     fn parse_const_block(&mut self, span: Span, pat: bool) -> PResult<'a, P<Expr>> {
         if pat {
             self.psess.gated_spans.gate(sym::inline_const_pat, span);
         }
-        self.expect_keyword(kw::Const)?;
         let (attrs, blk) = self.parse_inner_attrs_and_block()?;
         let anon_const = AnonConst {
             id: DUMMY_NODE_ID,
