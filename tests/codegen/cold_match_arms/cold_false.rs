9d7d8468bae630cad09832a035c051e13c01fa23
Jiri Bobek <jiri.bobek@gmail.com>
2024-01-23 00:32:00 +0100

    initial tests

---
+++ tests/codegen/cold_match_arms/cold_false.rs
@@ -0,0 +1,30 @@
+// compile-flags: -O
+#![crate_type = "lib"]
+
+#[inline(never)]
+#[no_mangle]
+pub fn hot_function() {
+    println!("hot");
+}
+
+#[inline(never)]
+#[no_mangle]
+pub fn cold_function() {
+    println!("cold");
+}
+
+#[no_mangle]
+pub fn f(x: bool) {
+    match x {
+        true => hot_function(),
+        #[cold] false => cold_function(),
+    }
+}
+
+// CHECK-LABEL: @f(
+// CHECK: br i1 %x, label %bb2, label %bb1, !prof ![[NUM:[0-9]+]]
+// CHECK: bb1:
+// CHECK: cold_function
+// CHECK: bb2:
+// CHECK: hot_function
+// CHECK: ![[NUM]] = !{!"branch_weights", i32 2000, i32 1}
