12ca9850f77283f1d9bfb7b1c9d0dfb10d863395
Matt Weber <30441572+mweber15@users.noreply.github.com>
2024-03-02 02:05:29 -0500

    Restructure `compile-flags` for tests
    Optimization needs to be explicitly disabled now.

--- tests/codegen/issues/issue-98678-closure-coroutine.rs
+++ tests/codegen/issues/issue-98678-closure-coroutine.rs
@@ -1,8 +1,7 @@
 // This test verifies the accuracy of emitted file and line debuginfo metadata for closures and
 // coroutines.
 //
-//@ compile-flags: -C debuginfo=2 -Z debug-info-type-line-numbers=true
-#![crate_type = "lib"]
+//@ compile-flags: --crate-type=lib -Copt-level=0 -Cdebuginfo=2 -Zdebug-info-type-line-numbers=true
 #![feature(coroutines)]
 
 // ignore-tidy-linelength
@@ -11,12 +10,12 @@
 // MSVC: ![[#FILE:]] = !DIFile({{.*}}filename:{{.*}}\\issue-98678-closure-coroutine.rs{{".*}})
 
 pub fn foo() {
-    // NONMSVC: !DICompositeType({{.*"}}{closure_env#0}{{".*}}file: ![[#FILE]]{{.*}}line: [[# @LINE + 2]],
+    // NONMSVC-DAG: !DICompositeType({{.*"}}{closure_env#0}{{".*}}file: ![[#FILE]]{{.*}}line: [[# @LINE + 2]],
     // MSVC-DAG: !DICompositeType({{.*"}}closure_env$0{{".*}}file: ![[#FILE]]{{.*}}line: [[# @LINE + 1]],
     let closure = |x| x;
     closure(0);
 
-    // NONMSVC: !DICompositeType({{.*"[{]}}coroutine_env#1{{[}]".*}}file: ![[#FILE]]{{.*}}line: [[# @LINE + 2]],
-    // MSVC-DAG: !DICompositeType({{.*".*foo::}}coroutine_env$1>{{".*}}file: ![[#FILE]]{{.*}}line: [[# @LINE + 1]],
-    let coroutine = || yield 1;
+    // NONMSVC-DAG: !DISubprogram({{.*"[{]}}coroutine_env#1{{[}]".*}}file: ![[#FILE]]{{.*}}line: [[# @LINE + 2]],
+    // MSVC-DAG: !DISubprogram({{.*".*foo::}}coroutine_env$1>{{".*}}file: ![[#FILE]]{{.*}}line: [[# @LINE + 1]],
+    let _coroutine = || yield 1;
 }
