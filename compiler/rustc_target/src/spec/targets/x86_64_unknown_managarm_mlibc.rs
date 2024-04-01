6883e01ca7f47f30e7db1cb376e17d3c435c1ac0
no92 <leo@managarm.org>
2024-04-01 17:35:09 +0200

    fixup! compiler: Add `x86_64-unknown-managarm-mlibc` target

--- compiler/rustc_target/src/spec/targets/x86_64_unknown_managarm_mlibc.rs
+++ compiler/rustc_target/src/spec/targets/x86_64_unknown_managarm_mlibc.rs
@@ -5,8 +5,7 @@ pub fn target() -> Target {
     base.cpu = "x86-64".to_string().into();
     base.max_atomic_width = Some(64);
     base.add_pre_link_args(LinkerFlavor::Gnu(Cc::Yes, Lld::No), &["-m64"]);
-    // don't use probe-stack=inline-asm until rust-lang/rust#83139 is resolved.
-    base.stack_probes = StackProbeType::Call;
+    base.stack_probes = StackProbeType::Inline;
 
     Target {
         llvm_target: "x86_64-unknown-managarm-mlibc".to_string().into(),
