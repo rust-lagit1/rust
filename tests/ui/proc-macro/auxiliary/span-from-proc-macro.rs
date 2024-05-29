f10b7fc8b25e12787176a2eaa9511f0f01d98179
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-05-29 18:33:13 +0000

    Fix tests

--- tests/ui/proc-macro/auxiliary/span-from-proc-macro.rs
+++ tests/ui/proc-macro/auxiliary/span-from-proc-macro.rs
@@ -6,7 +6,6 @@
 #![crate_type = "proc-macro"]
 
 extern crate proc_macro;
-extern crate custom_quote;
 
 use proc_macro::{quote, TokenStream};
 
@@ -15,19 +14,12 @@ macro_rules! expand_to_quote {
         quote! {
             let bang_error: bool = 25;
         }
-    }
+    };
 }
 
 #[proc_macro]
 pub fn error_from_bang(_input: TokenStream) -> TokenStream {
     expand_to_quote!()
-}
-
-#[proc_macro]
-pub fn other_error_from_bang(_input: TokenStream) -> TokenStream {
-    custom_quote::custom_quote! {
-        my_ident
-    }
 }
 
 #[proc_macro_attribute]
