ef4394d1a16134cf446186607856f9a4100d5b24
Max Niederman <max@maxniederman.com>
2024-09-15 01:02:13 -0700

    add guard patterns to THIR

--- compiler/rustc_middle/src/thir/visit.rs
+++ compiler/rustc_middle/src/thir/visit.rs
@@ -259,5 +259,9 @@ pub fn walk_pat<'thir, 'tcx: 'thir, V: Visitor<'thir, 'tcx>>(
                 visitor.visit_pat(pat);
             }
         }
+        Guard { subpattern, condition } => {
+            visitor.visit_pat(subpattern);
+            visitor.visit_expr(&visitor.thir()[*condition]);
+        }
     };
 }
