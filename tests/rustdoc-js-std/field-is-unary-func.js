b3e0da7f1fd22cbf39325257e580857142a4f99b
binarycat <binarycat@envs.net>
2024-10-16 20:08:55 -0500

    fix typo in search test

--- tests/rustdoc-js-std/field-is-unary-func.js
+++ tests/rustdoc-js-std/field-is-unary-func.js
@@ -1,6 +1,6 @@
 const EXPECTED = {
     // one of the only non-generic structs with public fields
-    'query': 'CpuidResult -> u32'
+    'query': 'CpuidResult -> u32',
     'others': [
         { 'path': 'core::arch::x86::CpuidResult', 'name': 'eax' },
     ],
